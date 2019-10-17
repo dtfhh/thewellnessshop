68862fd41d07499f4793782d7ae669fafc43132c
Anas Iqbal <im.anasiqbal@gmail.com>
2019-10-17 13:01:34 +0400

    Multisite Config

--- wp-config.php
+++ wp-config.php
@@ -79,6 +79,12 @@ $table_prefix = 'wp_';
  */
 define( 'WP_DEBUG', false );
 define('WP_ALLOW_MULTISITE', true);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'thewellnessshop.me');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
 
 /* That's all, stop editing! Happy publishing. */
 
